73029a097f48f6f27ab3a078fc8b0a02a203a6e1
Nikolay-Ra <55233955+Nikolay-Ra@users.noreply.github.com>
2019-11-30 16:00:19 +0300

    add api package

--- librenotes/settings.py
+++ librenotes/settings.py
@@ -8,6 +8,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "notes.apps.NotesConfig",
+    "rest_framework",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
