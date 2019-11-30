aa234bdcd0df3a986c3f4e88e7ac2c459d4cef33
Nikolay-Ra <55233955+Nikolay-Ra@users.noreply.github.com>
2019-11-30 15:19:14 +0300

    update models for sync

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -9,16 +11,20 @@ class User(AbstractUser):
 
 
 class Tag(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True)
     name = models.CharField(max_length=30)
     author = models.ForeignKey(get_user_model(), related_name="tags", on_delete=models.CASCADE)
+    updated = models.DateTimeField(auto_now=True)
+    deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.author}: {self.name}"
 
 
 class Note(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True)
     text = models.TextField()
-    tags = models.ManyToManyField(Tag, related_name="notes")
+    tags = models.ManyToManyField(Tag, blank=True, related_name="notes")
     author = models.ForeignKey(get_user_model(), related_name="notes", on_delete=models.CASCADE)
     created = models.DateTimeField(default=timezone.now)
     updated = models.DateTimeField(auto_now=True)
