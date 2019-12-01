e9405227f2d1df33605e00d7ac95c1b0ba5773d9
Nikolay-Ra <55233955+Nikolay-Ra@users.noreply.github.com>
2019-12-01 12:39:30 +0300

    register viewsets in router

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,9 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
+router.register("notes", views.NoteViewSet)
+router.register("tags", views.TagViewSet)
+router.register("sync", views.SyncViewSet, basename="Sync")
 
 urlpatterns = [
     path("", include(router.urls)),
